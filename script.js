3419c2f618b5629118508dfff4ea397584789a50
hadleyleng1979 <51281590+hadleyleng1979@users.noreply.github.com>
2022-07-15 16:14:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst", "Developer", "Auditor", "Machine Learning Enthusiast / Student"],
+        strings: ["Data Specialist", "Developer", "Auditor", "Machine Learning Enthusiast / Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analyst", "Developer", "Auditor", "Machine Learning Enthusiast / Student"],
+        strings: ["Data Specialist", "Developer", "Auditor", "Machine Learning Enthusiast / Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
