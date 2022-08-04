195cdf85b3bcaed24583dd524c5f1de00a389932
hadleyleng1979 <51281590+hadleyleng1979@users.noreply.github.com>
2022-08-04 20:59:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [" Data Design and Verification Manager", "Audit Manager", "Machine Learning Enthusiast / Student"],
+        strings: ["Data Design and Verification Manager", "Audit Manager", "Machine Learning Enthusiast / Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
